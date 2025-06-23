38bd9be1855879974f940351947d213345012ae7
LabAsim <lampgit@pm.me>
2025-06-23 21:53:00 +0300

    fix

--- tests/test-my-code.R
+++ tests/test-my-code.R
@@ -1,3 +1,8 @@
 test_that("2 + 2 equals 4", {
   expect_equal(2 + 2, 4)
 })
+
+
+# - name: Install dependencies
+#   run: |
+#     Rscript -e "install.packages('remotes');remotes::install_deps(dependencies = TRUE)"
